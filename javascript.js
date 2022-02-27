54a78424b3090693e71fcab88e9a4a73d1cba278
Asliddin <asd>
2022-02-27 13:59:43 +0300

    adsf

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var str = prompt('Ismingizni yozing')
+console.log("Sizning ismingiz " + str);
+
+var second = prompt('Famaliangizni kiriting')
+console.log("Sizning familiyangiz " + second);
+
+var yosh = prompt("Sizningiz yoshingiz nechida?")
+console.log('Yosh ingiz' + yosh)
+
+var num1 = prompt("3 + 4 =?")
+
+console.log( "3 + 4 =" + (3 + 4) + " Sizning javobingingiz: " + num1);
+
+var num2 = prompt("3 * 4 =?")
+
+console.log( "3 * 4 =" + (3 * 4) + " Sizning javobingingiz: " + num2);
+var num3 = prompt("3 / 4 =?")
+
+console.log( "3 / 4 =" + (3 / 4) + " Sizning javobingingiz: " + num3);
+var num4 = prompt("8 + 4 =?")
+
+console.log( "8 + 4 =" + (8 + 4) + " Sizning javobingingiz: " + num4);
+
+var num5 = prompt("3 + 9 =?")
+console.log( "3 + 9 =" + (3 + 9) + " Sizning javobingingiz: " + num5);
+
+var num5 = prompt("3 % 9 =?")
+console.log( "3 % 9 =" + (3 % 9) + " Sizning javobingingiz: " + num5);
+
+var con = alert("Consolga kiring!")
+
